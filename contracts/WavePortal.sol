6870fcd110f6374eec5e270101a79a481e46f77b
abarcadavid <davidabarca.swe@protonmail.com>
2023-06-20 11:07:08 -0700

    Contract can increase waves and return total waves

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,7 +5,15 @@ pragma solidity ^0.8.18;
 import "hardhat/console.sol";
 
 contract WavePortal {
-    constructor() {
-        console.log("I am a smart contract!");
+    uint256 totalWaves;
+
+    function wave() public {
+        totalWaves += 1;
+        console.log("%s has waved!", msg.sender);
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d total waves!", totalWaves);
+        return totalWaves;
     }
 }
