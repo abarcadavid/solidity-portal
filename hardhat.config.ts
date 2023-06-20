644c87682d7c020b63946478fe249578f55991c1
abarcadavid <davidabarca.swe@protonmail.com>
2023-06-19 19:52:44 -0700

    Add hre task to print list of accounts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,13 @@ import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-chai-matchers";
 import "@nomicfoundation/hardhat-ethers";
 
+task("accounts", "Prints the list of accounts", async (taskargs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+})
 
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
