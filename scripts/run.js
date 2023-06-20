0d8c472a18647935081c96f1e38c8d08b200f1dc
abarcadavid <davidabarca.swe@protonmail.com>
2023-06-20 12:21:27 -0700

    Add function calls to script to test contract

--- scripts/run.js
+++ scripts/run.js
@@ -1,7 +1,23 @@
 const main = async () => {
+  const [owner, randomPerson] = await hre.ethers.getSigners();
   const waveContract = await hre.ethers.deployContract("WavePortal");
   await waveContract.waitForDeployment();
   console.log("Contract deployed to:", waveContract.target);
+  console.log("Contract deployed by: ", owner.address);
+
+  await waveContract.getTotalWaves();
+
+  const wavetxn = await waveContract.wave();
+  await wavetxn.wait();
+
+  await waveContract.getTotalWaves();
+
+  const secondTxn = await waveContract.connect(randomPerson).wave();
+  console.log("This is the secondTxn object: ", secondTxn);
+  await secondTxn.wait();
+
+  await waveContract.getTotalWaves();
+
 };
 
 main().catch((error) => {
