fa62d746ec0205619fce5e5a6162208faa170a37
abarcadavid <davidabarca.swe@protonmail.com>
2023-06-20 18:59:56 -0700

    Configure hardhat to use Sepolia test network

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,6 +2,7 @@ import { HardhatUserConfig, task } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-chai-matchers";
 import "@nomicfoundation/hardhat-ethers";
+require("dotenv").config();
 
 task("accounts", "Prints the list of accounts", async (taskargs, hre) => {
   const accounts = await hre.ethers.getSigners();
@@ -13,6 +14,12 @@ task("accounts", "Prints the list of accounts", async (taskargs, hre) => {
 
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
+  networks: {
+    sepolia: {
+      url: process.env.STAGING_ALCHEMY_KEY!,
+      accounts: [process.env.PRIVATE_KEY!],
+    },
+  }
 };
 
 export default config;
