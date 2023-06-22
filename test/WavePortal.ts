a6e3e6c15bea1a0b20022b54073305690acfa1da
abarcadavid <davidabarca.swe@protonmail.com>
2023-06-22 14:39:25 -0700

    Test if waves array is populated correctly

--- test/WavePortal.ts
+++ test/WavePortal.ts
@@ -2,7 +2,7 @@ import { expect } from "chai";
 import { ethers } from "hardhat";
 import { loadFixture, time } from "@nomicfoundation/hardhat-network-helpers";
 
-describe("WavePortal", function() {
+describe("WavePortal Contract Tests", function() {
   // A fixture allows us to reuse the same contract setup in every test.
   // We run the contract setup once, snapshot the state,
   // and then reset Hardhat Network to that snapshot in every test
@@ -21,6 +21,23 @@ describe("WavePortal", function() {
       expect(wavePortalContract.runner!.address).to.equal(owner.address);
     })
   });
+
+  describe("Correct Data", function() {
+    it("Should Increase the Number of Waves", async function() {
+      const { wavePortalContract } = await loadFixture(deployWavePortalFixture);
+
+      expect(await wavePortalContract.getTotalWaves()).to.equal(0);
+      await wavePortalContract.wave("Hello");
+      expect(await wavePortalContract.getTotalWaves()).to.equal(1);
+    });
+
+    it("Should return the waves array", async function() {
+      const { wavePortalContract } = await loadFixture(deployWavePortalFixture);
+      expect(await wavePortalContract.getWaves()).to.have.lengthOf(0);
+      await wavePortalContract.wave("Stored messaged");
+      expect(await wavePortalContract.getWaves()).to.have.lengthOf(1);
+    })
+  })
 
   describe("Emit Events", function() {
     it("Should emit the Wave event triggered by Owner", async function() {
