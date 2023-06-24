68314aa3e3e03a9b44e4c6706eb8c040651efe31
abarcadavid <davidabarca.swe@protonmail.com>
2023-06-24 12:56:16 -0700

    Make contract payable

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -7,6 +7,10 @@ import "hardhat/console.sol";
 contract WavePortal {
     uint256 totalWaves;
 
+    constructor() payable {
+        console.log("Contract was built...");
+    }
+
     // Event will store arguements into transaction log. This notifies
     // external users(frontend) that a change has occured on the blockchain
     // when we decided to 'emit' this event
@@ -31,6 +35,20 @@ contract WavePortal {
         // After changing the blockchain, we will emit a signal to our frontend
         // and let it know that the smart contract has updated it's state
         emit newWave(msg.sender, block.timestamp, _message);
+
+        // Everyone who waves will receieve a prize amount of eth
+        uint256 prizeAmount = 0.0001 ether;
+
+        // We must require that the balance of the contract is > than prize amount
+        // If contract has less than prize amount, print error message.
+        require(
+            prizeAmount <= address(this).balance,
+            "Trying to withdraw more money than the contract has."
+        );
+
+        // Require that the user who made wave() call is successful in receiving prize
+        (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+        require(success, "Failed to withdraw money from the contract.");
     }
 
     function getWaves() public view returns (Wave[] memory) {
